fd8e844be125ff40b620a9ffc6c2d01d38fe846b
Andry <andryhughes@gmail.com>
2026-05-31 11:19:49 -0500

    fix: double-quote Sam's Club to escape apostrophe

--- components/scanner/ScannerTab.tsx
+++ components/scanner/ScannerTab.tsx
@@ -1,7 +1,7 @@
 ﻿'use client'
 import { useState, useRef, useEffect } from 'react'
 
-const STORES = ['Kroger','Walmart','HEB','Target','Aldi','Costco','Sam''s Club','Safeway','Randalls']
+const STORES = ['Kroger','Walmart','HEB','Target','Aldi','Costco',"Sam's Club",'Safeway','Randalls']
 const STORAGE_KEY = 'leanspend-scans'
 
 type Scan = {
